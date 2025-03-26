054f19675e9f0eee4541772b0bc796e67923880d
Maximilian Hense <maximilian.hense@outlook.com>
2025-03-26 13:57:05 +0100

    Testing with pylint

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,7 +1,48 @@
+import os
+
 import pytest
 
-from app.core import check_database_connection
+from sqlmodel import SQLModel, create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from testcontainers.core.waiting_utils import wait_for_logs
+from testcontainers.postgres import PostgresContainer
+from fastapi.testclient import TestClient
 
-@pytest.mark.integration
-def test_database_connection(db):
-    assert check_database_connection(), "Database connection failed"
+from app.model import *
+from app.main import app
+
+client = TestClient(app)
+
+@pytest.fixture(scope="session")
+def session(request):
+    '''
+    Setup postGIS container
+    '''
+    postgis = PostgresContainer(
+        "postgis/postgis:latest",
+        "5432",
+        "postgres",
+        "postgres",
+        "population",
+    )
+
+    postgis.start()
+
+    os.environ["DBURL"] = postgis.get_connection_url()
+    
+    engine = create_engine(os.getenv("DBURL"), echo=False)
+    session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
+
+    SQLModel.metadata.create_all(engine)
+    
+    def stop_db():
+        postgis.stop()
+
+    request.addfinalizer(stop_db)
+
+    return session
+    
+def test_read_main(session):
+    response = client.get("/test")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
