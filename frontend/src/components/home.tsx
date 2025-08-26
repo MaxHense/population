ca34608df327a6861581900b20b3f6827a050a4e
Maximilian Hense <maximilian.hense@outlook.com>
2025-08-26 15:16:41 +0200

    feat: Simple German Map in Frontend

---
+++ frontend/src/components/home.tsx
@@ -0,0 +1,16 @@
+import GermanySvg from '../assets/germany.svg';
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <img 
+        src={GermanySvg} 
+        alt="SVG Image" 
+        className="w-64 h-64"
+      />
+    </div>
+  );
+};
+
+export default Home;
