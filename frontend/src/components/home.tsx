9c036668064beaac3c9c8234b386f121d04d9203
Maximilian Hense <maximilian.hense@outlook.com>
2025-08-27 22:08:37 +0200

    feat: hover effect for border and adding title and picture to index.html

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,13 +1,16 @@
 import GermanySvg from '../assets/germany.svg';
+import { motion } from 'framer-motion'
 import React from 'react';
 
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <img 
-        src={GermanySvg} 
-        alt="SVG Image" 
-        className="w-64 h-64"
+      <motion.img
+        src={GermanySvg}
+        alt="SVG Image"
+        className="w-64 h-64 cursor-pointer"
+        whileHover={{ scale: 1.05 }}
+        transition={{ type: "spring", stiffness: 200, damping: 15 }}
       />
     </div>
   );
